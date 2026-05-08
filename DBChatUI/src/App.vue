d921eeef87644c1171cabccb3fa73d70cf2109f0
Saravana Kumar <srvnkmr@gmail.com>
2026-05-08 15:47:43 +0530

    admin ui start

--- DBChatUI/src/App.vue
+++ DBChatUI/src/App.vue
@@ -1,11 +1,10 @@
 <template>
 <LoginView v-if="!auth.isLoggedIn" />
-<HomeView v-else />
+    <router-view v-else />
 </template>
 
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth'
 import LoginView from '@/components/LoginView.vue'
-import HomeView from './views/HomeView.vue'
 const auth = useAuthStore()
 </script>
--- DBChatUI/src/router/index.ts
+++ DBChatUI/src/router/index.ts
@@ -1,9 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import adminRoutes from '@/admin/router/adminRoutes'
+
+import HomeView from '@/views/HomeView.vue'
+
 const router = createRouter({
+
   history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
+
     ...adminRoutes,
+
+    {
+      path: '/',
+      component: HomeView
+    }
   ],
 })
 
